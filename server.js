518f022a9830c30bbea6010e85ccdbbc337af2c2
ayzel <andrewzlee@gmail.com>
2023-06-22 11:21:45 -0400

    slash brokie
    for some reason adding a slash to the end of my site for cors broke it lmfao

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors({ origin: 'https://prismatic-daifuku-97fb97.netlify.app/', }));
+app.use(cors({ origin: 'https://prismatic-daifuku-97fb97.netlify.app', }));
 
 const port = process.env.PORT || 5000;
 const District = require('./district');
